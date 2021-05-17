bffe830bc6907237ec2849634a86854fbd9b2f1a
Guilherme Vieira <glvs011@gmail.com>
2021-05-17 20:03:01 -0300

    feat: create register page.

---
+++ components/templates/RegisterTemplate.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="register-template">
+    <NuxtLink to="/login" class="back-to-login">
+      <fa :icon="['fas', 'angle-left']" /> Voltar
+    </NuxtLink>
+    <h1>Registre-se na plataforma</h1>
+    <FormRegister />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+.register-template {
+  margin-top: 2rem;
+  .back-to-login {
+    display: flex;
+    align-items: center;
+    color: color('primaryDark');
+    text-decoration: none;
+    svg {
+      margin-right: 0.5rem;
+    }
+  }
+}
+</style>
