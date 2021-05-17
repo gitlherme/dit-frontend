7de1a3d6dc17eaf4bedef3b71f6b577b7e58f583
Guilherme Vieira <glvs011@gmail.com>
2021-05-17 03:28:52 -0300

    feat: create two with logo layout.

--- layouts/two-with-logo.vue
+++ layouts/two-with-logo.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="two-with-logo-layout">
-    <div class="left-side"></div>
-    <div class="right-side"></div>
+    <div class="left-side">
+      <img
+        src="../assets/images/logos/pink-dot-white.svg"
+        alt="Logo da DIT na cor pink com branco"
+      />
+    </div>
+    <div class="right-side">
+      <img
+        src="../assets/images/logos/full-darkest-blue.svg"
+        alt="Logo da DIT totalmente azul"
+        class="logo-darkest-blue"
+      />
+      <Nuxt />
+    </div>
   </div>
 </template>
 
@@ -9,14 +21,28 @@
 .two-with-logo-layout {
   width: 100%;
   height: 100vh;
+  display: flex;
   .left-side {
     width: 60%;
     height: 100%;
-    background-color: color('blue');
+    background-color: color('primaryDark');
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 400px;
+    }
   }
   .right-side {
     width: 40%;
     height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    .logo-darkest-blue {
+      width: 100px;
+    }
   }
 }
 </style>
---
+++ pages/login/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>TOP</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  layout: 'two-with-logo',
+})
+</script>
