d04df88563d6a8b8de5c47f92117f6cdf80e7b0e
gitlherme <dev@guilhermevieira.dev>
2021-09-27 21:36:59 -0300

    feat(user): initialize register module

--- store/users/register.ts
+++ store/users/register.ts
@@ -2,8 +2,12 @@ import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
 import { $axios } from '@/utils/nuxt-instance'
 
 interface CreatePayload {
-  description: string,
+  email: string,
   redirectUrl: string
+}
+
+interface ShowPayload {
+  key: string
 }
 
 @Module({ name: 'users/register', stateFactory: true, namespaced: true})
@@ -14,8 +18,9 @@ export default class UserRegister extends VuexModule {
   }
 
   @Action
-  public async show() {
-
+  public async show(payload: ShowPayload) {
+    const user = await $axios.get(`/users/register/${payload}`)
+    return user
   }
 
   @Action
